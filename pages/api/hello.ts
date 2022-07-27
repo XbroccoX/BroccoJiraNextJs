213e138a07bb11c74a4dd41a07f7b698e84ce627
brocco <saaristizabalco@gmail.com>
2022-07-27 10:44:05 -0500

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  ok: boolean;
+  message: string;
+  method: string;
+  secret?: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+  console.log(process.env)
+
+  res.status(200).json({
+    ok: true,
+    message: 'Todo es correcto',
+    method: req.method || 'no hay method',
+    secret: process.env.NEXT_PUBLIC_CLIENT_KEY
+  })
 }
